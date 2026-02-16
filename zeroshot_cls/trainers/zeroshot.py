098a8bb057377586dc74b0188e652a3bdabd1e36
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-17 00:15:34 +0330

    fix

--- zeroshot_cls/trainers/zeroshot.py
+++ zeroshot_cls/trainers/zeroshot.py
@@ -8,6 +8,11 @@ from trainers.graph_handler import aggergator_Graph
 from dassl.engine import TRAINER_REGISTRY, TrainerX
 from dassl.optim import build_optimizer, build_lr_scheduler
 
+def accuracy(output, target, topk=(1,)):
+    pred = output.topk(max(topk), 1, True, True)[1].t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
+
 class Textual_Encoder(nn.Module):
     def __init__(self, cfg, classnames, clip_model):
         super().__init__()
