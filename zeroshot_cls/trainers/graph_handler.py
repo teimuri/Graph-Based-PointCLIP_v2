e48a5fc22ab6c877607ee850c7c73a46375cacf0
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-16 22:36:16 +0330

    fix

--- zeroshot_cls/trainers/graph_handler.py
+++ zeroshot_cls/trainers/graph_handler.py
@@ -17,6 +17,7 @@ class aggergator_Graph(nn.Module):
         """
         x: Image features of shape [Batch * Num_Views, Channels]
         """
+        x = x.cuda()
         # 1. Message Passing: Let views talk to their neighbors
         # We process the whole batch of graphs at once
         # Create a batch vector to keep track of which nodes belong to which object in the batch
@@ -40,7 +41,7 @@ class aggergator_Graph(nn.Module):
         # Global mean pool averages the nodes for each graph in the batch
         aggr_feat = global_mean_pool(x, batch_idx) 
         
-        return aggr_feat
+        return aggr_feat.cpu()
 
     def get_view_edge_index(self):
         # Define the connections based on geometric proximity
