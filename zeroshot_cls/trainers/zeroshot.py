fcc7fae23825aeb78a0384a1f087f6f9120ce531
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-17 00:14:38 +0330

    fix

--- zeroshot_cls/trainers/zeroshot.py
+++ zeroshot_cls/trainers/zeroshot.py
@@ -127,7 +127,7 @@ class PointCLIPV2_ZS(TrainerX):
     def forward_backward(self, batch):
         # 1. Unpack the batch from the DataLoader
         print("BATCH KEYS:", batch.keys())
-        pc = batch["pointcloud"].cuda()
+        pc = batch["img"].cuda()
         label = batch["label"].cuda()
         batch_size = pc.shape[0]
 
