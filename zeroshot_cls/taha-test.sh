57fdcde01d29cbbb57564d1a39101afe8d22ff3b
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-16 17:29:50 +0330

    Added taha test and updated git ignore

---
+++ zeroshot_cls/taha-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Path to dataset
+DATASET=modelnet40
+# DATASET=scanobjectnn
+
+TRAINER=PointCLIPV2_ZS
+# Trainer configs: rn50, rn101, vit_b32 or vit_b16
+CFG=vit_b16
+
+export CUDA_VISIBLE_DEVICES=0
+python main.py \
+--trainer ${TRAINER} \
+--dataset-config-file configs/datasets/${DATASET}.yaml \
+--config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+--output-dir output/${TRAINER}/${CFG}/${DATASET} \
+--zero-shot \
+--post-search
