364a4d765e47d91db1568383d8bf29cc3039dd9a
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-17 18:28:01 +0330

    test

--- zeroshot_cls/main.py
+++ zeroshot_cls/main.py
@@ -120,6 +120,7 @@ def main(args):
         
         # 2. Extract the dataloader and total epochs
         train_loader = trainer.train_loader_x
+        raise ValueError(train_loader.dataset.transform)
         max_epochs = cfg.OPTIM.MAX_EPOCH
         for epoch in range(max_epochs):
             print(f"\n--- Epoch {epoch + 1}/{max_epochs} ---")
