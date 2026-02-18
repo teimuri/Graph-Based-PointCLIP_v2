1317883d1c2297a1405fd5d9d222d4444de91e91
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-18 15:14:15 +0330

    fix

--- zeroshot_cls/taha-test.sh
+++ zeroshot_cls/taha-test.sh
@@ -4,6 +4,8 @@ git pull
 # DATASET=modelnet40
 DATASET=scanobjectnn
 
+TARGETDATASET=modelnet40
+
 TRAINER=PointCLIPV2_ZS
 # Trainer configs: rn50, rn101, vit_b32 or vit_b16
 CFG=vit_b16
@@ -18,12 +20,10 @@ python main.py \
 
 #!/bin/bash
 
-# Path to dataset
-DATASET=modelnet40
-# DATASET=scanobjectnn
 
-TARGETDATASET=scanobjectnn
-# TARGETDATASET=modelnet40
+
+# Path to dataset
+# DATASET=scanobjectnn
 
 TRAINER=PointCLIPV2_ZS
 # Trainer configs: rn50, rn101, vit_b32 or vit_b16
@@ -32,10 +32,10 @@ CFG=vit_b16
 export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --trainer ${TRAINER} \
---dataset-config-file configs/datasets/${DATASET}.yaml \
+--dataset-config-file configs/datasets/${TARGETDATASET}.yaml \
 --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
---output-dir output/${TRAINER}/${CFG}/${DATASET} \
+--output-dir output/${TRAINER}/${CFG}/${TARGETDATASET} \
 --no-train \
 --zero-shot \
---gnn-dir output/${TRAINER}/${CFG}/${TARGETDATASET}/gnn/my_gnn_model.pth \
+--gnn-dir output/${TRAINER}/${CFG}/${DATASET}/gnn/my_gnn_model.pth \
 --post-search
