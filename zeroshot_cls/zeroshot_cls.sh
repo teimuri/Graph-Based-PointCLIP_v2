2605718518933eab0946a5832ee1eb86a7234be0
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2026-02-19 16:10:19 +0330

    fix

--- zeroshot_cls/zeroshot_cls.sh
+++ zeroshot_cls/zeroshot_cls.sh
@@ -4,6 +4,9 @@
 DATASET=scanobjectnn
 TARGETDATASET=modelnet40
 
+# Path to dataset
+# DATASET=scanobjectnn
+
 TRAINER=PointCLIPV2_ZS
 # Trainer configs: rn50, rn101, vit_b32 or vit_b16
 CFG=vit_b16
@@ -11,10 +14,10 @@ CFG=vit_b16
 export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --trainer ${TRAINER} \
---dataset-config-file configs/datasets/${DATASET}.yaml \
+--dataset-config-file configs/datasets/${TARGETDATASET}.yaml \
 --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
---output-dir output/${TRAINER}/${CFG}/${DATASET} \
+--output-dir output/${TRAINER}/${CFG}/${TARGETDATASET} \
 --no-train \
 --zero-shot \
---gnn-dir output/${TRAINER}/${CFG}/${TARGETDATASET}/gnn/my_gnn_model.pth \
+--gnn-dir output/${TRAINER}/${CFG}/${DATASET}/gnn/my_gnn_model.pth \
 --post-search
